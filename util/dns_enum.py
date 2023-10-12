58f912d7b5b0ba9fa3a871d53c0853b8adfcfafe
Niklas Myrefelt <niklas.myrefelt@fortnox.se>
2023-10-12 11:40:55 +0200

    Support for custom wordlists
    * Fix for connection timeout during TLS check.
    * Streamline JSON printing

--- util/dns_enum.py
+++ util/dns_enum.py
@@ -3,10 +3,14 @@ import socket
 import subprocess
 import sys
 import nmap3 #https://pypi.org/project/python3-nmap/
+from os import access, R_OK
+from os.path import isfile
 
-def dns_enum(target):
+def dns_enum(target, wordlist):
     nmap = nmap3.Nmap()
-    results = nmap.nmap_dns_brute_script(target, dns_brute="--script dns-brute.nse --script-args dns-brute.hostlist=./subdomains.txt")
+    assert isfile(wordlist) and access(wordlist, R_OK), \
+        f"Wordlist {wordlist} doesn't exist or isn't readable"
+    results = nmap.nmap_dns_brute_script(target, dns_brute="--script dns-brute.nse --script-args dns-brute.hostlist=" + wordlist)
     # Add any A-records for the original target as well.
     try:
         for ip in socket.gethostbyname_ex(target)[2]:
